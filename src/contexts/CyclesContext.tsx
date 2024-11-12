510bdca682670982e3482867fb51212cf6694ffa
Ricardo Sobral <riki.sobral@gmail.com>
2024-11-12 20:07:16 -0300

    refact: chance maps for immer lib

--- src/contexts/CyclesContext.tsx
+++ src/contexts/CyclesContext.tsx
@@ -1,5 +1,6 @@
-import { Circle } from "phosphor-react";
 import { createContext, ReactNode, useReducer, useState } from "react";
+import { addNewCyclesAction, interruptCurrentCycleAction, markCurrentCycleAsFinishedAction } from "../reducers/cycles/actions";
+import { Cycle, cyclesReducer } from "../reducers/cycles/reducer";
 // vamos criar aqui uma interface nova, para caso um dia mudemos a forma de validação que é pelo zod não sofremos muitos problemas, então por esse motivo é recriado
 
 interface CreateCycleData {
@@ -7,14 +8,7 @@ interface CreateCycleData {
   minutesAmount: number;
 }
 
-interface Cycle {
-  id: string;
-  task: string;
-  minutesAmount: number;
-  startDate: Date;
-  interruptDate?: Date;
-  finishedDate?: Date;
-}
+
 
 interface CyclesContextType {
   cycles: Cycle[];
@@ -34,10 +28,6 @@ interface CyclesContextProviderProps {
   children: ReactNode;
 }
 
-interface CyclesState {
-  cycles: Cycle[]
-  activeCycleId: string | null
-}
 
 export function CyclesContextProvider({
   children,
@@ -45,65 +35,18 @@ export function CyclesContextProvider({
   // o setCycles, ele vai ser metodo para disparar ações e não mais alterar diretamente o valor de   cycles, então vamos mudar esse nome para dispatch
   // o state, é o valor real (no momento) do nosso estado e uma, ele pode receber diversas informações
   // action, ela é qual ação o usuário ta querendo utilizar de alteração dentro da nossa variável, ações que o usuário pode fazer para alterar esse nosso "estádo"
-  const [cyclesState, dispatch] = useReducer((state: CyclesState, action: any) => {
-    switch (action.type) {
-      case "ADD_NEW_CYCLE":
-        return {
-          ...state,
-          cycles: [...state.cycles, action.payload.newCycle],
-          activeCycleId: action.payload.newCycle.id
-        };
-
-      case "INTERRUPT_CURRENT_CYCLE":
-        return {
-          ...state,
-          activeCycleId: null,
-          cycles: state.cycles.map((cycle) => {
-            if (cycle.id === state.activeCycleId) {
-              return {
-                ...cycle,
-                interruptDate: new Date(),
-              };
-            } else {
-              return cycle;
-            }
-          })
-        }
-      case "MARK_CURRENT_CYCLE_AS_FINISHED":
-        return {
-          ...state,
-          activeCycleId: null,
-          cycles: state.cycles.map((cycle) => {
-            if (cycle.id === state.activeCycleId) {
-              return { ...cycle, finishedDate: new Date() }
-            } else {
-              return cycle
-            }
-          })
-        }
-      default:
-        return state
-    }
-  },
+  const [cyclesState, dispatch] = useReducer(cyclesReducer,
     {
       cycles: [],
       activeCycleId: null
     }
   );
+
   const [amountSecondsPassed, setAmountSecondsPassed] = useState(0);
 
   const { cycles, activeCycleId } = cyclesState
 
   const activeCycle = cycles.find((cycle: Cycle) => cycle.id === activeCycleId);
-
-  function markCurrentCycleAsFinished() {
-    dispatch({
-      type: "MARK_CURRENT_CYCLE_AS_FINISHED",
-      payload: {
-        activeCycleId,
-      },
-    });
-  }
 
   // vamos criar um proxy, uma função que chama outra função
   function setSecondsPassed(seconds: number) {
@@ -122,24 +65,18 @@ export function CyclesContextProvider({
 
     // quando o dispatch é chamado, a função la no reducer vai ser executada, e a função que vamos passar como argumento do dispatch ela vai ser executada, ela será o action
     // é comum que para a utilização do reducer nos enviemos um objeto, dentro dele um type, contendo o nome da função/ação em caixa alta, e em sequencia o payload
-    dispatch({
-      type: "ADD_NEW_CYCLE",
-      payload: {
-        newCycle,
-      },
-    });
+    dispatch(addNewCyclesAction(newCycle));
     // aqui a baixo vamos colocar 0 para que o ciclo não comece onde o anterior parou, isso resolve um bug, se não nunca voltaria do inicio a contagem
     setAmountSecondsPassed(0);
     // tiramos o reset daqui para coloca rna home, pois o contexto deve depender apenas dele e não de nenhuma biblioteca
   }
 
   function interruptCurrentCycle() {
-    dispatch({
-      type: "INTERRUPT_CURRENT_CYCLE",
-      payload: {
-        activeCycleId,
-      },
-    });
+    dispatch(interruptCurrentCycleAction());
+  }
+
+  function markCurrentCycleAsFinished() {
+    dispatch(markCurrentCycleAsFinishedAction());
   }
 
   return (
